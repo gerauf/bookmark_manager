fcac2851d21a2fece39c61a98b966b16bd071180
gerauf <fo4786@gmail.com>
2016-05-06 15:35:34 +0100

    password error msg

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 
@@ -9,6 +10,8 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
+  register Sinatra::Flash
+
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
@@ -16,17 +19,23 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/' do
-    redirect :'signup'
+    redirect :'/users/new'
   end
 
-  get '/signup' do
-    erb :signup
+  get '/users/new' do
+    @user = User.new
+    erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/links'
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/links'
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   get '/links' do
